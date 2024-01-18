f51587524ff10a53b0a578a40f0175b38006f277
yogen <yogenstha520@gmail.com>
2024-01-18 15:06:53 +0545

    update

--- day_2.py
+++ day_2.py
@@ -25,22 +25,38 @@
 # #print(fruit1)
 
 #inherentance
-class Music:
-    def playMusic():
-        return "music has been started"
+# class Music:
+#     def playMusic():
+#         return "music has been started"
 
-class Guitar(Music):
-    def guitarMusic():
-        return "Jhing Jhing Jhak"
+# class Guitar(Music):
+#     def guitarMusic():
+#         return "Jhing Jhing Jhak"
 
-guitar= Guitar()
-print(Guitar.playMusic())
-print(Guitar.guitarMusic())
+# guitar= Guitar()
+# print(Guitar.playMusic())
+# print(Guitar.guitarMusic())
 
-# static Method
-class StaticExample:
-    @staticmethod
-    def guitarMusic():
-        return "jhing jhing" 
+# # static Method
+# class StaticExample:
+#     @staticmethod
+#     def guitarMusic():
+#         return "jhing jhing" 
 
-print(StaticExample.guitarMusic())
+# print(StaticExample.guitarMusic())
+
+#encapulation
+class EncapulationClass:
+    def __init__(self,name,age):
+        self.name = name
+        self._age = age  #protected
+    def get_age(self):
+        return self._age
+    def set_age(self,age):
+        if age>0:
+            self._age = age
+
+my_entry = EncapulationClass('Hari',23)
+print(f"Name: {my_entry.name}")
+#print(f"age: {my_entry.age}") #invalid
+print(f"age: {my_entry.get_age()}")
